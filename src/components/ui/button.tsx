82e7521076f3f97ee659b9be7efca26ed5493741
fedeparrilla10 <federicoivanpd@gmail.com>
2025-05-12 14:58:33 +0200

    [update] new sections + refactor

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        tech: "bg-tertiary text-cream shadow-xs hover:bg-tertiary/90",
+        tech: "bg-tertiary hover:bg-tertiary/90 text-cream px-6 py-2 rounded-lg transition-all duration-300 shadow-md hover:shadow-lg",
         tech_reverse: "bg-cream text-tertiary shadow-xs hover:bg-cream/90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
